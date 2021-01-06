6e70a45542775ae25b7b84b7df2c3c821e025a41
santiago <santiago>
2021-01-06 20:40:39 -0300

    Change CSS price

--- client/src/components/views/Pricing/PricingHome.js
+++ client/src/components/views/Pricing/PricingHome.js
@@ -1,50 +1,68 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import StripeCheckout from 'react-stripe-checkout'
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import React from "react";
+import ReactDOM from "react-dom";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import 'react-toastify/dist/ReactToastify.css'
-import './stylePrice.css'
-
-toast.configure()
+toast.configure();
 
 function PricingHome() {
   const [product] = React.useState({
-    name: '|Cocinarte Premium|',
+    name: "|Cocinarte Premium|",
     price: 14,
-    description: 'LLeva la pasion por la cocina, a otro nivel.'
-  })
+    description: "LLeva la pasion por la cocina, a otro nivel.",
+  });
 
   async function handleToken(token, addresses) {
     const response = await axios.post(
-      'http://localhost:5000/api/checkout/cardPay',
+      "http://localhost:5000/api/checkout/cardPay",
       { token, product }
-    )
-    const { status } = response.data
-    console.log('Response:', response.data)
-    if (status === 'success') {
-      toast('Success! Check email for details', { type: 'success' })
+    );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("El pago fue realizado exitosamente", { type: "success" });
     } else {
-      toast('Something went wrong', { type: 'error' })
+      toast("Ocurrio un error durante la transaccion", { type: "error" });
     }
   }
 
   return (
-    <div className='container'>
-      <div className='product'>
+    <div className="">
+      <div className="d-flex justify-content-center productImg">
         <h1>{product.name}</h1>
-        <h3>Oferta exclusiva · ${product.price}</h3>
       </div>
+      <div className="d-flex justify-content-center">
+        <h3>Oferta exclusiva: ${product.price}</h3>
+      </div>
+
+      <div className="d-flex justify-content-center container containerImg"></div>
+      <div className="d-flex justify-content-center">
       <StripeCheckout
-        stripeKey='pk_test_51I3ODJGL81eAF97DdCWXll6O0t5gJWkgaVCVhNStDn7B2qGRSVuYDsE4eJ45rHKGQCoFMxLHClXYHG3aPpGQfkRr00selbXucj'
+        className="mt-4 w-25"
+        label="Tarjeta"
+        stripeKey="pk_test_51I3ODJGL81eAF97DdCWXll6O0t5gJWkgaVCVhNStDn7B2qGRSVuYDsE4eJ45rHKGQCoFMxLHClXYHG3aPpGQfkRr00selbXucj"
         token={handleToken}
         amount={product.price * 100}
-        name='|Cocinarte Premium|'
+        name="|Cocinarte Premium|"
         billingAddress
       />
+      </div>
+
+      <div className="d-flex justify-content-center">
+      <StripeCheckout
+        className="mt-4 w-25"
+        label="Mercado pago"
+        stripeKey="pk_test_51I3ODJGL81eAF97DdCWXll6O0t5gJWkgaVCVhNStDn7B2qGRSVuYDsE4eJ45rHKGQCoFMxLHClXYHG3aPpGQfkRr00selbXucj"
+        token={handleToken}
+        amount={product.price * 100}
+        name="|Cocinarte Premium|"
+        billingAddress
+      />
+      </div>
     </div>
-  )
+  );
 }
 
-export default PricingHome
+export default PricingHome;
