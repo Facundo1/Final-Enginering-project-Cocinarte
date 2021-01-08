8cd22d597d1926446cc37820276b30465178e301
santiago <santiago>
2021-01-08 08:53:17 -0300

    Responsive page

--- client/src/components/views/Recipes/SingleRecipe.js
+++ client/src/components/views/Recipes/SingleRecipe.js
@@ -1,49 +1,49 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { fetchRecipes } from '../../../_actions/recipe_actions'
-import Favorite from './Favorite'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchRecipes } from "../../../_actions/recipe_actions";
+import Favorite from "./Favorite";
 
 class SingleRecipe extends Component {
   constructor(props) {
-    super(props)
-    const id = this.props.match.params.id
+    super(props);
+    const id = this.props.match.params.id;
     this.state = {
       recipe: {},
       id,
-      loading: true
-    }
+      loading: true,
+    };
   }
 
   async componentDidMount() {
     try {
-      await this.props.fetchRecipes()
+      await this.props.fetchRecipes();
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 
   componentDidUpdate() {
     if (!this.state.recipe.title && this.props.recipes) {
       const detailRecipe = this.props.recipes.find(
-        x => x._id.toString() === this.state.id.toString()
-      )
+        (x) => x._id.toString() === this.state.id.toString()
+      );
 
       if (detailRecipe) {
         this.setState({
           recipe: detailRecipe,
-          loading: false
-        })
+          loading: false,
+        });
       }
     }
   }
 
   render() {
-    const recipe = this.state.recipe
-    const id = this.state.recipe._id
-    console.log(id)
-    const Ingredients = recipe.Ingredients && recipe.Ingredients.split(',')
-    const Steps = recipe.Steps && recipe.Steps.split('-')
+    const recipe = this.state.recipe;
+    const id = this.state.recipe._id;
+    console.log(id);
+    const Ingredients = recipe.Ingredients && recipe.Ingredients.split(",");
+    const Steps = recipe.Steps && recipe.Steps.split("-");
     if (this.state.loading) {
       return (
         <div>
@@ -53,70 +53,82 @@ class SingleRecipe extends Component {
             </div>
           </div>
         </div>
-      )
+      );
     }
     return (
       <div>
         <div>
-          <div id='RecipeImageContainer'>
-            
-            <h3 className='titleReceta rounded mt-3'>
+          <div className="d-flex justify-content-center">
+            <h3 className="titleReceta rounded mt-3">
               <strong>{recipe.title}</strong>
             </h3>
+          </div>
+          <div className="d-flex justify-content-center">
             <img
               src={`http://localhost:5000/${recipe.photo}`}
-              id='imgRecipe'
-              style={{ maxHeight: '30rem' }}
-              alt='recipe'
+              style={{ maxHeight: "30rem" }}
+              alt="recipe"
             />
           </div>
           <div>
             <Favorite
-              userFrom={localStorage.getItem('userId')}
+              userFrom={localStorage.getItem("userId")}
               recipeId={id}
               recipeInfo={recipe}
             />
           </div>
           {/* info */}
-          <div id='DetallesRecetaContainer'>
+          <div
+            className="d-flex justify-content-center mt-4"
+          >
             <h3>
               <strong>{recipe.description}</strong>
             </h3>
-
+          </div>
+          <div className="d-flex justify-content-center mt-4">
             <ul>
-              <h2 className='titleIngredientes rounded'>
+              <h2 className="titleIngredientes rounded">
                 <strong>Ingredientes</strong>
               </h2>
               {Ingredients.map((item, index) => {
                 return (
-                  <li className='font-weight-bold h5' key={index}>
+                  <li className="font-weight-bold h5" key={index}>
                     {item}
                   </li>
-                )
+                );
               })}
             </ul>
-            <h3 className='titlePasos rounded mb-3'>
+          </div>
+          <div className="d-flex justify-content-center">
+            <h3 className="titlePasos rounded mt-4 mb-3">
               <strong>Pasos</strong>
             </h3>
+          </div>
+          <div className="d-flex justify-content-center">
             <ul>
               {Steps.map((item, index) => {
                 return (
-                  <li className='font-weight-bold text-left h5' key={index}>
+                  <li className="font-weight-bold text-left h5" key={index}>
                     {item}
                   </li>
-                )
+                );
               })}
             </ul>
-
-            <h3 className='titleCategoria rounded'>
-              <strong>{'Categoria de la receta'}</strong>
+          </div>
+          <div className="d-flex justify-content-center">
+            <h3 className="titleCategoria rounded">
+              <strong>{"Categoria de la receta"}</strong>
             </h3>
+          </div>
+          <div className="d-flex justify-content-center">
             <h4>
               <strong>{recipe.Category}</strong>
             </h4>
+          </div>
+          <div className="d-flex justify-content-center">
             {
-              <Link to='/Recetas' id='btnVolver'>
-                <button className='mt-5 btn btn-info  text-white rounded h5'>
+              <Link to="/Recetas" id="btnVolver">
+                <button className="mt-5 btn btn-info  text-white rounded h5">
                   Volver
                 </button>
               </Link>
@@ -124,14 +136,14 @@ class SingleRecipe extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({
-  recipes: state.recipe.items
-})
+const mapStateToProps = (state) => ({
+  recipes: state.recipe.items,
+});
 
 export default connect(mapStateToProps, {
-  fetchRecipes
-})(SingleRecipe)
+  fetchRecipes,
+})(SingleRecipe);
