609ae4e72f1cd12af03d40f7e8fa9f24f462d665
santiago <santiago.ottolini97@gmail.com>
2021-02-12 09:51:19 -0300

    Slider images

--- client/src/components/views/LandingPage/Home.js
+++ client/src/components/views/LandingPage/Home.js
@@ -1,24 +1,46 @@
-import React, { Component } from 'react'
-import Header from '../LandingPage/Header'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import Header from "../LandingPage/Header";
+import { Link } from "react-router-dom";
+import imgOne from "../../../assets/defaultBcg.jpeg";
+import imgTwo from "../../../assets/homeBcg.jpeg";
+import imgThree from "../../../assets/679975.jpg";
+import imgFour from "../../../assets/Four.jpg";
+import imgFive from "../../../assets/Five.jpg";
 
 function Home() {
   return (
     <>
       <div>
-        <Header title=''>
-          <div id='ContainerHome'>
-            <h1>|Cocinarte|</h1>
-            <Link to='Recetas'>
-              <button className='btn btn-info  text-white rounded h5'>
+        <Header title="">
+          <div id="ContainerHome">
+            <div className="slider">
+              <ul>
+              <li>
+                  <img src={imgFive}></img>
+                </li>
+                <li>
+                  <img src={imgTwo}></img>
+                </li>
+                <li>
+                  <img src={imgThree}></img>
+                </li>
+                <li>
+                  <img src={imgFour}></img>
+                </li>
+
+              </ul>
+            </div>
+           {/*  <h1>|Cocinarte|</h1>
+            <Link to="Recetas">
+              <button className="btn btn-info  text-white rounded h5">
                 Buscar Ingredientes
               </button>
-            </Link>
+            </Link> */}
           </div>
         </Header>
       </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
